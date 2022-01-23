616a006871669fdd66dd2430b5146b9cb8d0d3c4
Noah Peterson <noahbpeterson1997@gmail.com>
2022-01-22 18:17:03 -0600

    Removing logging, fixing issue with bot endlessly repeating itself.

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const mineflayer = require('mineflayer')
+const mineflayer = require('mineflayer');
 
 const bot = mineflayer.createBot({
     host: 'localhost', // minecraft server ip
@@ -7,15 +7,13 @@ const bot = mineflayer.createBot({
     // port: 25565,                // only set if you need a port that isn't 25565
     // version: false,             // only set if you need a specific version or snapshot (ie: "1.8.9" or "1.16.5"), otherwise it's set automatically
     // auth: 'mojang'              // only set if you need microsoft auth, then set this to 'microsoft'
-  })
+  });
   
   bot.on('chat', (username, message) => {
     if (username === bot.username) return;
-
-    console.log(username+" "+bot.username);
-    bot.chat(message)
-  })
+    bot.chat(message);
+  });
   
   // Log errors and kick reasons:
-  bot.on('kicked', console.log)
-  bot.on('error', console.log)
+  bot.on('kicked', console.log);
+  bot.on('error', console.log);
